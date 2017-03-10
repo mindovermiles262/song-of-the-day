666d0d73cd51667a717c7c7ba432d5c26a3e270a
Erik Johnson <mindovermiles262@gmail.com>
2017-03-10 14:26:37 -0800

    update 03/10

--- thecurrent.rb
+++ thecurrent.rb
@@ -1,5 +1,19 @@
 ## Updates The Current Song of the Day
 
-import 'nokogiri'
-import 'open-uri'
+# TODO: Change write location to beginning of file
 
+require 'nokogiri'
+require 'open-uri'
+require 'date'
+
+today = Date::MONTHNAMES[Date.today.month] + " " + Date.today.day.to_s + ", " + Date.today.year.to_s # => "March 10, 2017"
+
+f = File.open('./historic/TheCurrentSOTD.txt', 'a+')
+page = Nokogiri::HTML(open('http://www.thecurrent.org/collection/song-of-the-day'))
+page_date = page.css('time') #get all <time> tags
+page_date.each do |line|
+    if line.text == today
+        f.write(line.ancestors.xpath('h2').text.strip! + "\n")
+    end
+end
+f.close
