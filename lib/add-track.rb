7a51ccc52198c9f585346016ed2b585318eec068
Erik Johnson <mindovermiles262@gmail.com>
2017-03-12 00:16:52 -0800

    add rescue for logs

--- lib/add-track.rb
+++ lib/add-track.rb
@@ -43,7 +43,11 @@ def add_track(track_uri)
         puts "Error code #{response.code}. Track not added to Spotify."
         @error_log[:track_uri] = [Time.new, response.code, response.body]
     end
-    log = File.new("./log/error_log_#{Time.new.to_s.gsub(" ","_")[0..18]}.log", "w")
+    begin
+        log = File.new("./log/error_log_#{Time.new.to_s.gsub(" ","_")[0..18]}.log", "w")
+    rescue
+        log = File.new("./../log/error_log_#{Time.new.to_s.gsub(" ","_")[0..18]}.log", "w")
+    end
     log.write(@error_log)
     log.close
 end
