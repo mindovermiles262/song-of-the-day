ee5f0272a5c8c66d785dc262ff2b9835c4842274
Erik Johnson <mindovermiles262@gmail.com>
2017-03-12 12:52:29 -0700

    KEXP Deploy

--- lib/add-historic-KEXP.rb
+++ lib/add-historic-KEXP.rb
@@ -12,9 +12,16 @@ File.open('./../data/KEXP-SOTD.txt') {|f| count = f.read.count("\n")}
 # Method add_track adds songs to top of playlist so adding tracks bottom up keeps playlist in order
 while count > 0
     track = IO.readlines('./../data/KEXP-SOTD.txt')[count-1]
-    uri = get_track_uri(track)
-    puts "Fetched URI for track '#{track}"
-    add_track(uri)
-    count -= 1
+    begin
+        uri = get_track_uri(track)
+        puts "Fetched URI for track '#{track}"
+        add_track(uri)
+        count -= 1
+    rescue
+        log = File.new("../log/invalid_uri_log_#{Time.new.to_s.gsub(" ","_")[0..18]}.log", "w")
+        log.write(track)
+        log.close
+        count -= 1
+    end
 end
 f.close
