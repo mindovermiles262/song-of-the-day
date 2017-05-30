2915f3f82b4b1c4315eb5ba2610398a38abadea1
Andy Duss <mindovermiles262@gmail.com>
2017-05-30 07:41:07 -0700

    As of 05/29/17 Spotify requires OAuth token to search Library.  Fixed get-track-uri.rb to include token in search request.  Updated search to include gem rest-client.

--- lib/get-track-uri.rb
+++ lib/get-track-uri.rb
@@ -1,38 +1,41 @@
 # Get Spotify Track URI from search query
 def get_track_uri(search_query)
-    require 'net/http'
     require 'uri'
     require 'json'
-    require 'i18n' # Converts UTF-8 Chars into ASCII for URI Search Request
+    require 'i18n'
+    require 'rest-client'
+    require_relative '../conf/configure'
 
+    # Set Playlist ID and User Token
+    config = configure
+    playlist_id = config[:playlist_id]
+    token = config[:user_token]
+
+
+    # Converts UTF-8 Chars into ASCII for URI Search
     I18n.available_locales = [:en]
-
     query = I18n.transliterate(search_query.strip.gsub(/\(feat.+.*/,'').gsub(' ','+').gsub('&','')) #change search_query spaces to '+'
 
-    #puts "Query: #{query}" # Debugging
+    # puts "Query: #{query}" # Debugging
 
     # query Spotify Web API
-    search = URI.parse("https://api.spotify.com/v1/search?q=" + query + "&type=track&limit=1")
-    request = Net::HTTP::Get.new(search)
-    request["Accept"] = "application/json"
-
-    req_options = { use_ssl: search.scheme == "https", }
-
-    response = Net::HTTP.start(search.hostname, search.port, req_options) do |http|
-        http.request(request)
-    end
+    response = RestClient.get 'https://api.spotify.com/v1/search?q=' + query + '&type=track', {Accept: 'application/json', 'Authorization' => 'Bearer ' + token}
 
     # Search the response for track URI
     resp = JSON.parse(response.body)
     resp = resp['tracks'].to_s.split(" ")
     track_uri = String.new
     resp.each do |line|
+        # TODO: Rewrite using Regex
         if line.include?('uri"=>"spotify:track:')
             track_uri = line[8..-5]
         end
     end
 
+    # Debugging Console Outputs
+    # puts "Resonse: #{response}"
     puts "Spotify Response: #{response.code}"
+    # puts "Track URI: #{track_uri}"
 
     if track_uri.length > 3
         # puts "Track URI: #{track_uri}"
